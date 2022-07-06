6191f531c86f5891d3375d81c2b00b62a8214241
JadeSass <pelumiezekiel.developer@gmail.com>
2022-07-06 13:52:03 +0100

    created connected account with transaction context

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
       development: {
        host: "127.0.0.1",     // Localhost (default: none)
        port: 7545,
-       url: 'http://127.0.0.1:8545',    // Standard Ethereum port (default: none)
+       url: 'HTTP://127.0.0.1:7545',            // Standard Ethereum port (default: none)
        network_id: "*"        // Any network (default: none)
       }
   }
