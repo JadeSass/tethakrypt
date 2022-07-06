289945d68c2b44705159e5d71902d0a71fa2c8cb
JadeSass <pelumiezekiel.developer@gmail.com>
2022-07-06 11:51:03 +0100

    pragma -v init

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -1,12 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
-/// @title A title that should describe the contract/interface
-/// @author The name of the author
-/// @notice Explain to an end user what this does
-/// @dev Explain to a developer any extra details
-
 contract Transactions {
-    uint256 = transactionCounter;
+    uint256 transactionCounter;
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
