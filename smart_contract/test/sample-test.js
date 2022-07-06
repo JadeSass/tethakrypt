de8fb8cd710b5f6dddc836f5bc75611daac617f9
JadeSass <pelumiezekiel.developer@gmail.com>
2022-07-06 13:26:04 +0100

    test

--- smart_contract/test/sample-test.js
+++ smart_contract/test/sample-test.js
@@ -13,7 +13,7 @@ const main = async () => {
 const runMain = async () =>{
   try {
     await main();
-    process.print("Transactions deployed to:", transactions.address);
+    process.exit(0);
   } catch (error) {
     console.error(error);
     process.exit(1);
